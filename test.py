e6765a762b0581ce95ad3d1180bde0f3ecdb762d
skey <37370495+shuykang@users.noreply.github.com>
2023-05-17 22:51:02 +0800

    Update test.py

--- test.py
+++ test.py
@@ -46,3 +46,31 @@ encrypt_file(key, 'myfile.bin', 'myfile_encrypted.bin')
 
 # 解密文件
 decrypt_file(key, 'myfile_encrypted.bin', 'myfile_decrypted.bin')
+
+
+
+
+import os
+
+# 定义要拆分的文件和每个文件的大小（单位为字节）
+input_file = 'myfile.bin'
+chunk_size = 1024 * 1024  # 每个文件大小为 1MB
+
+# 打开输入文件，读取数据并拆分为多个块
+with open(input_file, 'rb') as fin:
+    file_index = 0
+    while True:
+        chunk = fin.read(chunk_size)
+        if not chunk:
+            break
+        # 如果块大小不足 chunk_size，就用 0 补齐
+        if len(chunk) < chunk_size:
+            chunk += bytes(chunk_size - len(chunk))
+        # 写入新的文件
+        output_file = f'myfile_{file_index:03d}.bin'
+        with open(output_file, 'wb') as fout:
+            fout.write(chunk)
+        file_index += 1
+
+print(f'Split {input_file} into {file_index} files.')
+
